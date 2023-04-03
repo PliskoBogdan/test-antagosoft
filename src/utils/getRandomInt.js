ce0d0dc623db371a784e7071ab7522453227461a
PliskoBogdan <fanatamas64@gmail.com>
2023-04-03 16:36:29 +0300

    fix: do the getRandomInt method clear function

--- src/utils/getRandomInt.js
+++ src/utils/getRandomInt.js
@@ -1,5 +1,5 @@
 export default function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  const ceilMinVal = Math.ceil(min);
+  const floorMaxVal = Math.floor(max);
+  return Math.floor(Math.random() * (floorMaxVal - ceilMinVal + 1)) + ceilMinVal;
 }
